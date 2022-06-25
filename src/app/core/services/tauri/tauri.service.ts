b9d9c24d011997dfa23c5439b72937f11474238d
Joshua Dyer <dyerjo@mail.gvsu.edu>
2022-06-25 08:15:18 -0500

    first commit

---
+++ src/app/core/services/tauri/tauri.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { invoke } from '@tauri-apps/api/tauri';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TauriService {
+
+  constructor() {
+  }
+
+  get isTauri(): boolean {
+    return !!(window && window.__TAURI__);
+  }
+
+  async callHelloWorld() {
+    const text = await invoke('hello_world_command');
+    console.log('calling Hello World command from tauri.service.ts');
+    console.log(text);
+  }
+}
