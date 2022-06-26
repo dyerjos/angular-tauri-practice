15614e1b9d98308d73c5dff6350407fe5ea2eea0
Joshua Dyer <dyerjo@mail.gvsu.edu>
2022-06-26 10:24:02 -0500

    add services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,12 +16,14 @@ import { DetailModule } from './detail/detail.module';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 // AoT requires an exported function for factories
 const httpLoaderFactory = (http: HttpClient): TranslateHttpLoader =>  new TranslateHttpLoader(http, './assets/i18n/', '.json');
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent],
+  declarations: [AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent],
   imports: [
     BrowserModule,
     FormsModule,
